b9dbc331476b365628945ad180763b528304f95e
NivinCNC <nivincharles@gmail.com>
2026-06-26 19:36:58 +0530

    Minorfix dailymotion

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/Dailymotion.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/Dailymotion.kt
@@ -6,7 +6,8 @@ import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.newSubtitleFile
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import java.net.URI
 
 
@@ -41,7 +42,12 @@ open class Dailymotion : ExtractorApi() {
         meta.qualities?.get("auto")?.forEach { quality ->
             val videoUrl = quality.url
             if (!videoUrl.isNullOrEmpty() && videoUrl.contains(".m3u8")) {
-                getStream(videoUrl, this.name, callback)
+                callback.invoke(newExtractorLink(
+                    name,
+                    name,
+                    videoUrl,
+                    ExtractorLinkType.M3U8
+                ))
             }
         }
 
@@ -73,15 +79,6 @@ open class Dailymotion : ExtractorApi() {
         val id = path.substringAfter("/video/")
         return if (id.matches(videoIdRegex)) id else null
     }
-
-    private suspend fun getStream(
-        streamLink: String,
-        name: String,
-        callback: (ExtractorLink) -> Unit
-    ) {
-        return generateM3u8(name, streamLink, "").forEach(callback)
-    }
-
 
     data class MetaData(
         val qualities: Map<String, List<Quality>>?,
